822631523d13b1a96aab1517955491ad9507ea2b
Dair Aban <daban@inovazi.co>
2025-03-04 22:53:24 -0600

    Implemented adding new task

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,27 +2,8 @@ import express from "express"
 
 const router = express.Router();
 
-let tasks = [
-    {
-        id: 1,
-        title: "Task 1",
-        description: "This is a test task 1",
-        completed: false
-    },
-    {
-        id: 2,
-        title: "Task 2",
-        description: "This is a test task 2",
-        completed: true
-    },
-    {
-        id: 3,
-        title: "Task 3",
-        description: "This is a test task 3",
-        completed: false
-    }
-];
-let tasksCounter = 0;
+let tasks = [];
+let tasksCounter = 1;
 
 router.get("/", (req, res) => {
     res.render("tasks", {
@@ -31,5 +12,25 @@ router.get("/", (req, res) => {
     });
 });
 
+router.post("/add-task", (req, res) => {
+    var { title, description } = req.body;
+
+    if(!title || !description){
+        res.status(400).send('Invalid title or description!');
+    }
+
+    tasks.push(
+        {
+            id: tasksCounter,
+            title: title,
+            description: description,
+            completed: false
+        }
+    );
+
+    tasksCounter++;
+
+    res.redirect('/');
+});
 
 export default router
