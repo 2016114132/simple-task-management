fe5787daa4d05d1037b6e0d1c96ec75af2fd14c4
Dair Aban <daban@inovazi.co>
2025-03-04 22:41:46 -0600

    Created simple UI

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -2,8 +2,34 @@ import express from "express"
 
 const router = express.Router();
 
+let tasks = [
+    {
+        id: 1,
+        title: "Task 1",
+        description: "This is a test task 1",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Task 2",
+        description: "This is a test task 2",
+        completed: true
+    },
+    {
+        id: 3,
+        title: "Task 3",
+        description: "This is a test task 3",
+        completed: false
+    }
+];
+let tasksCounter = 0;
+
 router.get("/", (req, res) => {
-    res.send("Welcome!");
+    res.render("tasks", {
+        title: "Welcome!",
+        tasks: tasks
+    });
 });
+
 
 export default router
